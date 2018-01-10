1870a626d666e9a49924077622eed0dcd8329ba7
Christopher Sanders <astericky@gmail.com>
2018-01-09 20:04:10 -0500

    convert tabs to spaces

--- even_odd_project/main.go
+++ even_odd_project/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func main() {
-	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for _, number := range numbers {
-		if number % 2 == 0 {
-			fmt.Println(fmt.Sprint(number)+" is even")
-		} else {
-			fmt.Println(fmt.Sprint(number)+" is odd")
-		}
-	}
+    for _, number := range numbers {
+        if number % 2 == 0 {
+            fmt.Println(fmt.Sprint(number)+" is even")
+        } else {
+            fmt.Println(fmt.Sprint(number)+" is odd")
+        }
+    }
 }
